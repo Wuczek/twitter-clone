f8ce22a3b4353b279da071190641c99bc305f66e
Kamil Wójtowicz <93767137+Wuczek@users.noreply.github.com>
2023-11-21 14:30:50 +0100

    login component

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
+import Login from "./pages/Login";
 import "./index.css";
 
 export default function App() {
@@ -13,8 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
@@ -22,5 +20,5 @@ export default function App() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <form action="">
+      <input type="text" />
+      <button type="submit" className="border">
+        Siemanko
+      </button>
+    </form>
+  );
+};
+
+export default Login;
