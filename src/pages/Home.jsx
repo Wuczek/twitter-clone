74030053dddb032f08d3dcc0bb75394b4ec7c174
Kamil Wójtowicz <93767137+Wuczek@users.noreply.github.com>
2023-11-21 12:52:18 +0100

    added react-router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Post from "../components/Post";
+
+const Home = () => {
+  return (
+    <main className="mt-8 space-y-8 mb-4">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </main>
+  );
+};
+
+export default Home;
