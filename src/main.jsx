e541c530361149523f78f763eee511afe74d93b7
Kamil Wójtowicz <93767137+Wuczek@users.noreply.github.com>
2023-11-23 15:00:40 +0100

    fixed checkingSession

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import NoPage from "./pages/NoPage";
@@ -8,11 +9,34 @@ import Register from "./pages/Register";
 import "./index.css";
 
 export default function App() {
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/checkSession.php", {
+          method: "GET",
+          credentials: "include",
+        });
+
+        const data = await response.json();
+        if (!data.success) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        } else {
+          setIsLogged(true);
+        }
+      } catch {
+        console.log("Something went wrong");
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+        <Route path="/" element={<Layout isLogged={isLogged} setIsLogged={setIsLogged}/>}>
+          <Route index element={<Home isLogged={isLogged}/>} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="*" element={<NoPage />} />
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,34 +3,12 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import {useEffect,useState} from 'react'
 
-const Layout = () => {
-  const [isLogged, setIsLogged] = useState(false);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await fetch("http://localhost:8000/checkSession.php", {
-          method: "GET",
-          credentials: "include",
-        });
-
-        const data = await response.json();
-        if (!data.success) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        } else {
-          setIsLogged(true);
-        }
-      } catch {
-        console.log("Something went wrong");
-      }
-    };
-    fetchUser();
-  }, []);
+const Layout = ({setIsLogged,isLogged}) => {
 
   return (
     <>
       <Navbar isLogged={isLogged} setIsLogged={setIsLogged}/>
-      <Outlet isLogged={isLogged}/>
+      <Outlet />
       <Footer />
     </>
   );
