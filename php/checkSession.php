54549f6724ff193a235d268c7eadc3bf0bf894f6
michciook <michtabiznes@gmail.com>
2023-11-23 13:45:49 +0100

    backend for session handling

---
+++ php/checkSession.php
@@ -0,0 +1,11 @@
+<?php
+
+    session_start();
+    if(isset($_SESSION['user'])) {
+        $sessionInfo = array('user' => $_SESSION['user']);
+        echo json_encode($sessionInfo);
+    }
+
+    
+
+?>
