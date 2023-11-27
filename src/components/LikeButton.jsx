92dded1a02978b92c6886c3e8fe39498082e2831
Kamil Wójtowicz <93767137+Wuczek@users.noreply.github.com>
2023-11-27 18:26:36 +0100

    added LikeButton component

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,7 +1,28 @@
-const LikeButton = () => {
-  return (
-    <div>Like</div>
-  )
-}
+const LikeButton = ({ post_id }) => {
+  const handleLike = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/login.php", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `post_id=${post_id}`,
+      });
+      const data = await response.json();
 
-export default LikeButton
+      if (data.success) {
+        console.log("success");
+      } else {
+        console.log("fail");
+      }
+      
+    } catch (error) {
+      console.log("error");
+    }
+  };
+
+  return <button className="bg-red-400">Like</button>;
+};
+
+export default LikeButton;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { BsPerson } from "react-icons/bs";
+import LikeButton from "./LikeButton";
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
   return (
     <article className="shadow-xl border p-2 max-w-md mx-auto rounded-xl">
       <div className="flex items-center gap-2">
         <BsPerson />
         <p>{post.username}</p>
       </div>
-      <p>
-        {post.content}
-      </p>
+      <p>{post.content}</p>
+      <div className="flex gap-2">
+        <LikeButton post_id={post.id} />
+        <p>{post.like_count}</p>
+      </div>
     </article>
   );
 };
